3a7bbf75b47e13661181811e8f337adb10acad4b
Sue <1175634156@qq.com>
2023-10-16 12:55:34 +0800

    delete some functions that should not be added.

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -1,6 +1,12 @@
 package synthesizer;
 
-public class AbstractBoundedQueue {
+public abstract  class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     protected int fillCount;
     protected int capacity;
+    public int capacity() {
+        return capacity;
+    }
+    public int fillCount() {
+        return fillCount;
+    }
 }
