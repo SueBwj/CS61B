09de65f394868da8c08bf6b88986556db9d58b62
Sue <1175634156@qq.com>
2023-10-19 16:15:28 +0800

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,80 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-
+    private WeightedQuickUnionUF site;
+    private int N;
+    private int topSite;
+    private int bottomSite;
+    private int numOpen;
+    private boolean[][] flagOpen;
+    private int xyto1D(int x, int y) {
+        return x * N + y;
+    }
+    private void validateRange(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    private void unionNeighbor(int row, int col, int newRow, int newCol) {
+        if ((newRow >= 0 && newCol >= 0) && (newRow < N && newCol < N)) {
+            if (flagOpen[newRow][newCol]) {
+                site.union(xyto1D(row, col), xyto1D(newRow, newCol));
+            }
+        }
+    }
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+        topSite = N * N;    // 虚拟节点
+        bottomSite = N * N + 1; // 虚拟节点
+        site = new WeightedQuickUnionUF(N * N + 2); // +2是包含虚拟头底节点
+        flagOpen = new boolean[N][N];
+        // 将topSite与所有的头节点连接
+        for (int i = 0; i < N; i++) {
+            site.union(xyto1D(0, i), topSite);
+        }
+        // 将bottomSite与所有的尾节点连接
+        for (int i = 0; i < N; i++) {
+            site.union(xyto1D(N - 1, i), bottomSite);
+        }
+        // 所有的节点都是关闭的
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                flagOpen[i][j] = false;
+            }
+        }
+    }
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validateRange(row, col);
+        if (!flagOpen[row][col]) {
+            flagOpen[row][col] = true;
+            numOpen++;
+            unionNeighbor(row, col, row, col - 1);
+            unionNeighbor(row, col, row, col + 1);
+            unionNeighbor(row, col, row + 1, col);
+            unionNeighbor(row, col, row - 1, col);
+        }
+    }
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validateRange(row, col);
+        return flagOpen[row][col];
+    }
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validateRange(row, col);
+        return site.connected(xyto1D(row, col), topSite);
+    }
+    // number of open sites
+    public int numberOfOpenSites() {
+        return numOpen;
+    }
+    // does the system percolate?
+    public boolean percolates() {
+        return site.connected(topSite, bottomSite);
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,40 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
-
+    private int T; // 进行T次蒙特卡洛模拟
+    private double[] openFraction; // 满足渗透条件下打开格子的比例
+    private final double c = 1.96;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.T = T;
+        Percolation percolation = pf.make(N);
+        for (int i = 0; i < T; i++) {
+            while (!percolation.percolates()) {
+                int x;
+                int y;
+                do {
+                    x = StdRandom.uniform(N);
+                    y = StdRandom.uniform(N);
+                } while (percolation.isOpen(x, y));
+                percolation.open(x, y);
+            }
+            openFraction[i] = (double) percolation.numberOfOpenSites() / N * N;
+        }
+    }
+    public double mean() {
+        return StdStats.mean(openFraction);
+    }
+    public double stddev() {
+        return StdStats.stddev(openFraction);
+    }
+    public double confidenceLow() {
+        return mean() - c * stddev() / Math.sqrt(T);
+    }
+    public double confidenceHigh() {
+        return mean() + c * stddev() / Math.sqrt(T);
+    }
 }
